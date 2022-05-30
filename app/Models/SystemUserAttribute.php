84326224a28c098aef81cd470411aba1684e1299
Nobi <nobi@nobidev.com>
2022-05-30 15:38:36 +0000

    Add system_user_attribute model

---
+++ app/Models/SystemUserAttribute.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+class SystemUserAttribute extends BaseModel
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'key',
+        'cast_type',
+        'is_readonly',
+        'is_hidden',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'is_readonly' => 'boolean',
+        'is_hidden' => 'boolean',
+    ];
+}
