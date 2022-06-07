2762870bc61bec13633369bf22f08fb6a3f58540
Nobi <nobi@nobidev.com>
2022-06-07 17:28:08 +0000

    Add resource model

---
+++ app/Models/Resource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+class Resource extends BaseModel
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'namespace_id',
+        'path',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+    ];
+}
