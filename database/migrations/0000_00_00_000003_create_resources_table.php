2742de6886cdc8386d02e84f96fdab68214ab5ec
Nobi <nobi@nobidev.com>
2022-06-07 17:22:06 +0000

    Add resources table

---
+++ database/migrations/0000_00_00_000003_create_resources_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resources', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('namespace_id')->constrained('resource_namespaces');
+            $table->string('path');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unique(['namespace_id', 'path']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
