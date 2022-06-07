072e7028ea8bc169b6fcb2234cfdec0d1373fb5e
Nobi <nobi@nobidev.com>
2022-06-07 17:31:18 +0000

    Add resource seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(SystemSettingSeeder::class);
         $this->call(ResourceNamespaceSeeder::class);
+        $this->call(ResourceSeeder::class);
     }
 }
