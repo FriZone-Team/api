e1a65b789763050b177a97f8b4f7341c5fef58e2
Nobi <nobi@nobidev.com>
2022-06-16 02:07:11 +0000

    Add user_inboxes table

---
+++ database/migrations/0000_00_00_000023_create_user_inboxs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_inboxes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('owner_id')->constrained('users')->restrictOnUpdate()->cascadeOnDelete();
+            $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_inboxes');
+    }
+};
